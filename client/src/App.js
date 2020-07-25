1fdec19a81fa1f9c8e6afc50d06c3402ec499e91
Brijesh Dhanani <46484569+BRdhanani@users.noreply.github.com>
2020-07-25 18:55:31 +0530

    update app to make it reactize

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    const { title, body } = this.state;
+
+    event.preventDefault();
+
+    let payload = {
+      title,
+      body
+    }
+
+    axios({
+      url: '/blog/add',
+      method: 'POST',
+      data: payload
+    })
+    .then(() => {
+      console.log('Data sent');
+    })
+    .catch((error) => {
+      console.log('error');
+    })
+  }
+  render() {
+    const { title, body } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-input">
+          <input type="text" placeholder="Enter Title" name="title" value={title} onChange={this.handleChange}/>
+        </div>
+        <div className="form-input">
+          <textarea name="body" placeholder="Enter Description" value={body} onChange={this.handleChange}></textarea>
+        </div>
+        <button>Submit</button>
+      </form>
+    );
+  }
 }
 
 export default App;
